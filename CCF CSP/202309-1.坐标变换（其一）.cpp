8725c6613f53fb21d58bbd1ab192f867830eac18
xiamingxing733 <157434343+xiamingxing733@users.noreply.github.com>
2024-01-23 16:26:16 +0800

    Update 202309-1.坐标变换（其一）.cpp

--- CCF CSP/202309-1.坐标变换（其一）.cpp
+++ CCF CSP/202309-1.坐标变换（其一）.cpp
@@ -1,1 +1,25 @@
 # 坐标变换（其一）
+
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+int temp_x=0, temp_y=0;
+int x, y;
+int main()
+{
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x >> y;
+        temp_x += x;
+        temp_y += y;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> x >> y;
+        x += temp_x;
+    	y += temp_y;
+    	cout << x <<" "<< y << "\n"; 
+    }
+    return 0;
+}
